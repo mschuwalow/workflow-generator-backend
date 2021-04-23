4042185dd706d27c08d0e7828224e7145fe4f12d
Maxim Schuwalow <16665913+mschuwalow@users.noreply.github.com>
2021-04-23 21:19:25 +0200

    Update doobie-core, doobie-hikari, ... to 0.13.0 (#43)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val betterMonadicFor       = "0.3.1"
     val circe                  = "0.13.0"
-    val doobie                 = "0.10.0"
+    val doobie                 = "0.13.0"
     val flyway                 = "7.3.2"
     val http4s                 = "0.21.19"
     val jawn                   = "1.1.1"
