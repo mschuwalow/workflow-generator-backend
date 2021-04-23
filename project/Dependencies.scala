0aa00b3320419b82724c79b654e5ca3ab59e2035
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-04-23 19:23:01 +0000

    Update flyway-core to 7.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val betterMonadicFor       = "0.3.1"
     val circe                  = "0.13.0"
     val doobie                 = "0.13.0"
-    val flyway                 = "7.3.2"
+    val flyway                 = "7.8.1"
     val http4s                 = "0.21.19"
     val jawn                   = "1.1.1"
     val kindProjector          = "0.11.3"
