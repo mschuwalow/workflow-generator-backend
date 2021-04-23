0cd6ce21587fb55ee43b1458817fe5f4c83a1391
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-04-23 19:11:08 +0000

    Update zio, zio-streams, zio-test, ... to 1.0.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val silencer               = "1.7.0"
     val sttp                   = "2.2.9"
     val tsec                   = "0.2.1"
-    val zio                    = "1.0.5"
+    val zio                    = "1.0.7"
     val zioInteropCats         = "2.4.0.0"
     val zioKafka               = "0.14.0"
     val zioLogging             = "0.5.8"
