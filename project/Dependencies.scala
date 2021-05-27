4fef77d4b550e435306e114565060a53a71282f5
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-05-27 01:51:12 +0000

    Update circe-core, circe-generic, ... to 0.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val betterMonadicFor       = "0.3.1"
-    val circe                  = "0.13.0"
+    val circe                  = "0.14.1"
     val doobie                 = "0.13.4"
     val flyway                 = "7.9.0"
     val http4s                 = "0.21.23"
