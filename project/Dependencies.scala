b89320586221741dddc334dc89f42f294208356f
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2023-02-15 00:28:24 +0000

    Update zio-logging, zio-logging-slf4j to 0.5.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val zio                    = "1.0.17"
     val zioInteropCats         = "2.5.1.0"
     val zioKafka               = "0.14.0"
-    val zioLogging             = "0.5.14"
+    val zioLogging             = "0.5.15"
     val zioPrelude             = "1.0.0-RC5"
   }
   import Versions._
